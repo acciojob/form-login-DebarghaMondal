4611cea9f723e021b79bff86dd4d7618454686ef
DebarghaMondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-07-19 10:24:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue() {   
+	let firstname = document.getElementById("f-name").value;
+	let lastname = document.getElementById("l-name").value;
+	let fullname = firstname + " " + lastname;
+alert(fullname);
 }
